2da02869bdcb0094dab2dccdd471e0b6145ee716
Vladimir Kolesnikov <__kolesnikov__@mail.ru>
2020-02-28 11:38:45 +0900

    create project

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public ItemType type;
+    public float attack;
+    public Sprite icon;
+
+    public float GetPrice()
+    {
+        return attack * 40;
+    }
+}
+
+public enum ItemType
+{
+    Dagger,
+    Axe,
+    Sword,
+    Staff,
+    Bow,
+    FireScroll
+}
---
+++ Assets/Scripts/ItemView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemView : MonoBehaviour
+{
+    public Button button;
+    public Image ItemIcon;
+
+    private ItemData itemData;
+
+    public void InitItem(ItemData item, Action<ItemData> callback)
+    {
+        itemData = item;
+        ItemIcon.sprite = itemData.icon;
+
+        button.onClick.AddListener(() => callback(itemData));
+    }
+
+    private void ButtonClicked()
+    {
+        Debug.Log(itemData.name);
+    }
+}
